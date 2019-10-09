2c827c48de6f0e380c726702481e0c511cd287a8
mechaadi <as808780@gmail.com>
2019-10-09 20:30:12 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      post: []
+    }
+    this.ref = firebase.firestore().collection('testItems')
+  }
+
+  componentDidMount(){
+    console.log("anything")
+    this.sun = this.ref.onSnapshot((snap)=>{
+      
+        const data = [];
+        snap.forEach((doc)=>{
+          console.log(doc);
+          data.push({
+            title: doc.data().title,
+            price : doc.data().price,
+           
+          })
+        });
+        this.setState({
+          post : data
+        });
+
+        console.log(this.state.post)
+       // const date1 = this.state.post[0].timestamp;
+    })
+  }
+
+  render() {
+    const listItems = this.state.post.map((item) =>
+  <div>
+ <h1>{item.title}</h1> 
+ <p>{item.price}</p>
+  </div>
+);
+
+    return(
+      listItems
+    )
+}
 }
 
 export default App;
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+
 import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrixfs7jFtvklpmhidK_uhr1O5xdaRgFM",
+    authDomain: "a7atestdatabase.firebaseapp.com",
+    databaseURL: "https://a7atestdatabase.firebaseio.com",
+    projectId: "a7atestdatabase",
+    storageBucket: "a7atestdatabase.appspot.com",
+    messagingSenderId: "857852480256",
+    appId: "1:857852480256:web:26bc8a07932acf541b81e5",
+    measurementId: "G-YN4CSD7PHD"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
